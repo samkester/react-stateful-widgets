2c2b1c80db747f1eb4cfa8c2183ce48d8522e869
Sam Kester <samkester@gmail.com>
2020-08-10 16:40:41 -0600

    TTT: CPU can make strategic moves

--- src/components/Tictactoe.js
+++ src/components/Tictactoe.js
@@ -4,6 +4,17 @@ const gameStateReady = "";
 const gameStatePlayerWin = "You won!";
 const gameStatCPUWin = "You lost!";
 
+const tilesInWin = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
 export default function Tictactoe() {
     const [gameSpaces, setGameSpaces] = useState([0,0,0,0,0,0,0,0,0]);
     const [gameState, setGameState] = useState(gameStateReady);
@@ -63,12 +74,54 @@ export default function Tictactoe() {
         //   non-stateful list to checkGameState makes sure that it's up to date.
 
         // CPU turn...
-        randomCPUMove(currentSpaces);
+        if(!strategicCPUMove(currentSpaces))
+        {
+            randomCPUMove(currentSpaces);
+        }
         // end CPU turn
-        console.log(currentSpaces);
+        //console.log(currentSpaces);
 
         setGameSpaces(currentSpaces);
         checkGameState(currentSpaces);
+    }
+
+    // this function returns TRUE if it was able to make a move, FALSE otherwise
+    function strategicCPUMove(currentSpaces){
+        const wins = winProgress(currentSpaces);
+
+        const canInterruptPlayerWin = [];
+
+        for(let i = 0; i < wins.length; i++){
+            if(wins[i] === -2){
+                playUnclaimedCPUMoveInWin(i, currentSpaces); // CPU can win with this move; play it immediately
+                return true;
+            }
+            else if(wins[i] === 2){
+                canInterruptPlayerWin.push(i); // player can win with this move; interrupt it if CPU can't win with any other move
+            }
+        }
+        if(canInterruptPlayerWin.length > 0)
+        {
+            // interrupt the first imminent player win, if any
+            // if there is more than one, CPU loses to perfect play anyway so don't worry about which one
+            playUnclaimedCPUMoveInWin(canInterruptPlayerWin[0], currentSpaces);
+            return true;
+        }
+
+        return false; // unable to make a strategic move, so fall back to a random one
+    }
+
+    function playUnclaimedCPUMoveInWin(win, currentSpaces){
+        // we have a number representing one of eight wins
+        // two of the three squares in that win are claimed and it's imperative we claim the last
+        for(let i = 0; i < 3; i++){
+            if(currentSpaces[tilesInWin[win][i]] === 0){
+                //if the ith space in the win we're examining is unclaimed...
+                currentSpaces[tilesInWin[win][i]] = -1; // claim it for the CPU
+            }
+        }
+        // by the rules of tic-tac-toe, it should be impossible for us to end up here without a third open spot to claim
+        // since that would require the sum of the win to be an odd number, but we got here by it being 2 or -2
     }
 
     function randomCPUMove(currentSpaces){
